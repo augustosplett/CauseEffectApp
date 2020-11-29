0c391f162e07d0d2312948896d319413beecdb93
augustosplett <augusto.splett@gmail.com>
2020-11-29 13:13:59 +0000

    basic structure created

---
+++ assets/features/index.js
@@ -0,0 +1,14 @@
+const people = [
+    {name: "Joao", street: "Burslem Street", city: "Porto Alegre", state: "RS", country: "Brazil", telephone: "99999999", birthday: "28/10/1987"},
+    {name: "Maria", street: "Snail's Bank", city: "Alegrete", state:"RS" , country: "Brazil", telephone: "88888888", birthday: "25/11/1990"},
+    {name: "Pedro", street: "Poolhill Close", city: "Tangamandápio", state: "RS", country: "Brazil", telephone: "77777777", birthday: "28/01/1970"},
+    {name: "Jose", street: "Brockenhurst Gardens", city: "Cachoeirinha", state: "RS", country: "Brazil", telephone: "66666666", birthday: "05/01/2000"},
+    {name: "Astolfo", street: "Sunnybank Terrace", city: "Anta Gorda", state: "RS", country: "Brazil", telephone: "55555555", birthday: "12/07/1995"}
+];
+
+
+function content(){
+    var body = document.querySelector(".container");
+    var nome = people;
+    body.innerHTML = nome;  
+}
